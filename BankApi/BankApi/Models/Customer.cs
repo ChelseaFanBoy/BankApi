67679a38aecf81e86db0a177faecf10de50e3f7c
ChelseaFanBoy <100678617+ChelseaFanBoy@users.noreply.github.com>
2022-03-06 13:31:59 +0530

    Adding the code for Web API

---
+++ BankApi/BankApi/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BankApi.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string CustomerName { get; set; } = null!;
+
+        public int CustomerID { get; set; }
+
+        public List<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
---
+++ BankApi/BankApi/Models/Enums/AccountType.cs
@@ -0,0 +1,11 @@
+﻿namespace BankApi.Models.Enums
+{
+    public enum AccountType
+    {
+        Savings = 1,
+        
+        Current = 2,
+
+        Salary = 3
+    }
+}
