bb3a7f894b92f61692ec4cbdfcf3ed4868e5ba0a
ChelseaFanBoy <navaneeth.1206033@srec.ac.in>
2022-03-14 19:36:00 +0530

    fixing api crash on run

---
+++ BankApi/BankApi/Services/BankService.cs
@@ -0,0 +1,36 @@
+using BankApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BankApi.Services
+{
+    public class BankService
+    {
+        private readonly IMongoCollection<Customer> _customerCollection;
+
+        public BankService(
+            IOptions<BankDatabaseSettings> bankDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                bankDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                bankDatabaseSettings.Value.DatabaseName);
+
+            _customerCollection = mongoDatabase.GetCollection<Customer>(
+                bankDatabaseSettings.Value.CustomerCollectionName);
+        }
+
+        public async Task<List<Customer>> GetAsync() =>
+            await _customerCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Customer?> GetAsync(int customerID) =>
+            await _customerCollection.Find(x => x.CustomerID == customerID).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Customer newCustomer) =>
+            await _customerCollection.InsertOneAsync(newCustomer);
+
+        public async Task UpdateAsync(int customerID, Customer updatedCustomer) =>
+            await _customerCollection.ReplaceOneAsync(x => x.CustomerID == customerID, updatedCustomer);
+    }
+}
